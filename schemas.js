ef2be63b778d2ae371c49861ee36bd88e31a1055
AkshayKohad <65406900+AkshayKohad@users.noreply.github.com>
2021-04-18 13:47:49 +0530

    Update schemas.js

--- schemas.js
+++ schemas.js
@@ -1,11 +1,42 @@
-const joi = require("joi")
+const BaseJoi = require("joi")
+const sanitizeHtml = require("sanitize-html")
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
+
 
 module.exports.productSchema = Joi.object({
     product: Joi.object({
-        rating: Joi.number().required().min(1),
-        imagePath: Joi.string().required(),
-        title: Joi.string().required(),
-        description: Joi.string().required() 
+        price: Joi.number().required().min(1),
+        imagePath: Joi.string().required().escapeHTML(),
+        title: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML() 
 
+    }).required()
+})
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required().escapeHTML()
     }).required()
 })
